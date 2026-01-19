1a5dd2275e8d287211b57e89f38fbeff09f3d6fd
Avik43218 <roychoudhuryavik005@gmail.com>
2026-01-19 18:54:41 +0530

    Hello World in x86_64 assembly

---
+++ hello_world.s
@@ -0,0 +1,20 @@
+; Hello-World program written in x86_64 Assembly language using Linux syscalls
+
+section .data
+	msg db "Hello World", 0xA
+	msg_len equ $ - msg
+
+section .text
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msg_len
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
